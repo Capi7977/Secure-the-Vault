5fb766da3ad64820983dfbfce985c2306267253e
Capi7977 <129705834+Capi7977@users.noreply.github.com>
2024-01-30 10:32:56 -0700

    create html and css

---
+++ sprit.js
@@ -0,0 +1,9 @@
+// First I assinged each varable that contains the corresponding result of calculation using a unique arithmetic operator
+let code1 = 5 + 5;
+let code2 = 80 - 40
+let code3 = 78 / 2
+
+// Next after finishing with step one I use the alert so that when a user logs into the window, an alert will pop out with an important message and an important code
+// Also I used the let name(code1, code2, code3) to let the computer know that I would like to put in the code in the alert message.
+alert('You have received this message because you have been chosen to open an important vault. Here is the secret combination: ' + code1 + " - " + code2 + "-" + code3)
+ 
